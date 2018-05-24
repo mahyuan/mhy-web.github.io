1ed054205e943ed90fb9b5698955a6febdccbe65
mhyuan <dsz9258@163.com>
2018-05-24 21:38:36 +0800

    增加 gulp配置

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+let gulp = require('gulp');
+let minifycss = require('gulp-minify-css');
+let uglify = require('gulp-uglify');
+let htmlmin = require('gulp-htmlmin');
+let htmlclean = require('gulp-htmlclean');
+let imagemin = require('gulp-imagemin');
+let clean = require('gulp-clean');
+let shell = require('gulp-shell');
+
+gulp.task('html', function() {
+	let options = {
+        removeComments: true, //清除HTML注释
+        collapseWhitespace: true, //压缩HTML
+        collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+        //removeScriptTypeAttributes: false, //删除<script>的type="text/javascript"
+        //removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+        minifyJS: true, //压缩页面JS
+        minifyCSS: true //压缩页面CSS
+    };
+	return gulp.src('./public/**/*.html')
+		.pipe(htmlclean())
+		.pipe(htmlmin(options))
+		.on('error', function(err) {
+			console.error('html error!', err);
+			this.end();
+		})
+		.pipe(gulp.dest('./public'))
+});
+
+gulp.task('js', function() {
+	return gulp.src('./public/**/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('./public'));
+});
+
+gulp.task('images', function() {
+	gulp.src('./public/img/*.*')
+		.pipe(imagemin({
+			progressive: true
+		}))
+		.pipe(gulp.dest('./public/img'));
+});
+
+gulp.task('css', function() {
+	return gulp.src('./public/**/*.css')
+		.pipe(minifycss())
+		.pipe(gulp.dest('./public'))
+});
+
+// CSS 文件已压缩，需要压缩的有html、js和图片
+const taskList = [
+	'html',
+	// 'css',
+	'js',
+	'images'
+	];
+
+gulp.task('build', taskList);
+
