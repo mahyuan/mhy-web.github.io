66d759a84d704f50a31e2cd7bbb2c0ba4f23ece2
mahy <dsz9258@126.com>
2019-01-10 16:32:54 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -5,52 +5,55 @@ var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 var imagemin = require('gulp-imagemin');
+var runSequence = require('run-sequence');
 
 gulp.task('minify-css', function () {
-	return gulp.src('./public/**/*.css')
-		.pipe(minifycss())
-		.pipe(gulp.dest('./public'));
+  return gulp.src('./public/**/*.css')
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('minify-html', function () {
-	return gulp.src('./public/**/*.html')
-		.pipe(htmlclean())
-		.pipe(htmlmin({
-			removeComments: true,
-			minifyJS: true,
-			minifyCSS: true,
-			minifyURLs: true,
-		}))
-		.pipe(gulp.dest('./public'))
+  return gulp.src('./public/**/*.html')
+    .pipe(htmlclean())
+    .pipe(htmlmin({
+      removeComments: true,
+      minifyJS: true,
+      minifyCSS: true,
+      minifyURLs: true,
+    }))
+    .pipe(gulp.dest('./public'))
 });
 
 gulp.task('minify-js', function () {
-	return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-		.pipe(babel({
-			presets: ['es2015']
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('./public'));
+  return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public'));
 });
 
 // 压缩图片
 gulp.task('minify-images', function () {
-	return gulp.src('./public/images/**/*.*')
-		.pipe(imagemin(
-			[imagemin.gifsicle({
-					'optimizationLevel': 3
-				}),
-				imagemin.jpegtran({
-					'progressive': true
-				}),
-				imagemin.optipng({
-					'optimizationLevel': 7
-				}),
-				imagemin.svgo()
-			], {
-				'verbose': true
-			}))
-		.pipe(gulp.dest('./public/images'))
+  return gulp.src('./public/images/**/*.*')
+    .pipe(imagemin(
+      [imagemin.gifsicle({
+          'optimizationLevel': 3
+        }),
+        imagemin.jpegtran({
+          'progressive': true
+        }),
+        imagemin.optipng({
+          'optimizationLevel': 7
+        }),
+        imagemin.svgo()
+      ], {
+        'verbose': true
+      }))
+    .pipe(gulp.dest('./public/images'))
 });
 
-gulp.task('default', ['minify-html', 'minify-css', 'minify-js']);
+gulp.task('default', function (cb) {
+  runSequence(['minify-html', 'minify-css', 'minify-js', 'minify-images'], cb);
+});
