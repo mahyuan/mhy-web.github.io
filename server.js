4066c78c70f8b252e2c4a6e56213a43cce3abce3
mhyuan <dsz9258@163.com>
2020-01-20 18:39:44 +0800

    Site updated: 2020-01-20 18:39:43

---
+++ server.js
@@ -0,0 +1,8 @@
+// const Koa = require('koa');
+// const path = require('path');
+// const serve = require('koa-static');
+// const main = serve(path.join(__dirname));
+// const app = new Koa();
+
+// app.use(main);
+// app.listen(3000);
